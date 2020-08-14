60ac2610730ffb03989c6befd4b5fb59f1e4438f
Mark Tiapkov <mark.tiapkov@gmail.com>
2020-08-14 11:04:43 -0700

    Create function for each language

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+//Business Logic
+//Write a function for each language, using math.round in order to display results via a math function
+
+function javaScript (num1, num2, num3) {
+  if (Math.round((num1 + num2 + num3)/3) === 1){
+    return true
+  }
+}
+function Python (num1, num2, num3) {
+  if (Math.round((num1, num2, num3)/3) === 2){
+    return true
+  }
+}
+function cSharp (num1, num2, num3) {
+  if (Math.round((num1, num2, num3)/3)=== 3){
+    return true
+  }
+}
