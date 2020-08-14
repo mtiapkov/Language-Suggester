d130d35504d217152b494f9b51783483067c0266
Mark Tiapkov <mark.tiapkov@gmail.com>
2020-08-14 11:35:22 -0700

    Begin adding User Interface Logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
 //Business Logic
-//Write a function for each language, using math.round in order to display results via a math function
-
 function javaScript (num1, num2, num3) {
   if (Math.round((num1 + num2 + num3)/3) === 1){
     return true
