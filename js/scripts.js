49a76c239d734967f245f8753dd1fd2e7605619d
Mark Tiapkov <mark.tiapkov@gmail.com>
2020-08-23 13:26:17 -0700

    add num4/5 to language functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 //Business Logic
 function javaScript (num1, num2, num3) {
-  if (Math.round((num1 + num2 + num3)/3) === 1){
+  if (Math.round((num1 + num2 + num3 + num4 + num5)/5) === 1){
     return true
   }
 }
 function Python (num1, num2, num3) {
-  if (Math.round((num1 + num2 + num3)/3) === 2){
+  if (Math.round((num1 + num2 + num3 + num4 + num5)/5) === 2){
     return true
   }
 }
 function cSharp (num1, num2, num3) {
-  if (Math.round((num1 + num2 + num3)/3)=== 3){
+  if (Math.round((num1 + num2 + num3 + num4 + num5)/5)=== 3){
     return true
   }
 }
